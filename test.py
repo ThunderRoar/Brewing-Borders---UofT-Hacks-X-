f1d691dd30154ed83c129abdbcf772551849613d
Vincent Trung <vincent.trung4@gmail.com>
2023-01-22 02:07:20 -0500

    ...

--- test.py
+++ test.py
@@ -2,12 +2,17 @@ from flask import Flask, request, render_template
 from cohesion import *
 
 app = Flask(__name__)
+the_data = ''
 
-
-@app.route("/")
+@app.route("/", methods = ['GET','POST'])
 def hello_world():
-    return render_template("Main.html")
+  if request.method == "POST":
+    print (request.form.get("str"))
+    the_data = request.form.get("str")
+    print(the_data)
+
+  return render_template("Main.html")
 
 @app.route("/page")
 def page2():
-    return render_template("Page2.html")
+    return render_template("Page2.html", data = the_data)
