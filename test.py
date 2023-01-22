e016a74dbff35b4a9ba92f792c082b50d9106763
James Liang <86000097+jamesliangg@users.noreply.github.com>
2023-01-21 19:57:13 -0500

    flask

--- test.py
+++ test.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return ""
+    return """<h1>How Do You Feel?</h1>
+    <div>
+    <input type="search" id="site-search" name="q">  
+    <button>Spill the Tea</button>
+    </div>"""
