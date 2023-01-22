46e440e015924af012a745cc4ffdee51818609b7
James Liang <86000097+jamesliangg@users.noreply.github.com>
2023-01-21 19:58:49 -0500

    Flask

--- test.py
+++ test.py
@@ -4,8 +4,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return """<h1>How Do You Feel?</h1>
+    return """<body>
+  <header>
+    <h1>How Do You Feel?</h1>
     <div>
     <input type="search" id="site-search" name="q">  
     <button>Spill the Tea</button>
-    </div>"""
+    </div>
+  </header>
+    <div>
+    <h2>Olong Tea</h2>
+    <img src="olong.jpg" alt="tea"
+    width=50% 
+    height=30% 
+    padding: 20px;
+    border-radius: 50%;
+    style="border: 60px solid #9AAE88";
+    class="longer-bottom-border"
+    />
+    </div>
+</body>"""
